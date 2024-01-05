2e6129a072225b8e9776751704079320bd6b5cc7
lavanya-mehrish <155656536+lavanya-mehrish@users.noreply.github.com>
2024-01-06 03:00:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
     // Smooth scrolling for anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
+            // Prevent the default behavior of the link
             e.preventDefault();
 
+            // Scroll to the target element smoothly
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
@@ -14,9 +17,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const darkModeToggle = document.getElementById('dark-mode-toggle');
     const body = document.body;
 
+    // Check if the darkModeToggle element exists
     if (darkModeToggle) {
+        // Add a click event listener to the darkModeToggle button
         darkModeToggle.addEventListener('click', () => {
+            // Toggle the 'dark-mode' class on the body element
             body.classList.toggle('dark-mode');
-        });
-    }
+        });
+    }
 });
